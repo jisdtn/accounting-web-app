160575c751cb21bb5679773a6ecc4855b1d176ce
jisdtn <122818010+jisdtn@users.noreply.github.com>
2024-06-19 12:07:34 +0200

    white space at the end of the file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from postgresql_app import models
-from postgresql_app.database import SessionLocal, engine
+# from postgresql_app import models
+# from postgresql_app.database import SessionLocal, engine
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -10,26 +10,29 @@ app = FastAPI()
 
 
 # Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 class ReadRootRequest(BaseModel):
     name: str
+
 
 @app.post("/")
 def read_root(request: ReadRootRequest):
     if request.name:
         return {f"Hello, {request.name}!"}
     else:
-        return {f"Hello, World!"}
+        return {"Hello, World!"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
 
 @app.get("/")
 async def root():

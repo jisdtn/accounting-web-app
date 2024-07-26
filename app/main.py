15657ec0bed23b7449cdb7b25f51d875066cdc58
jisdtn <122818010+jisdtn@users.noreply.github.com>
2024-07-26 14:10:00 +0200

    add methods and endpoints

--- app/main.py
+++ app/main.py
@@ -1,43 +1,136 @@
-from typing import Union
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from datetime import date
+
+from fastapi import FastAPI, HTTPException, Request, Query
 import asyncpg
+from typing import Optional
 
 
 app = FastAPI()
 
-
-async def get_db_connection():
-    return await asyncpg.connect(user='postgres', password='postgres', database='postgres', host='127.0.0.1')
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    conn = await get_db_connection()
-    try:
-        row = await conn.fetchrow('SELECT * FROM items WHERE id = $1', item_id)
-        if row is None:
-            raise HTTPException(status_code=404, detail="Item not found")
-        return dict(row)
-    finally:
-        await conn.close()
+DATABASE_URL = "postgresql://postgres:postgres@db:5432/postgres"
 
 
-class ReadRootRequest(BaseModel):
-    name: str
+async def get_pool() -> asyncpg.pool.Pool:
+    return await asyncpg.create_pool(DATABASE_URL, min_size=5, max_size=20)
+
+pool: Optional[asyncpg.pool.Pool] = None
+
+@app.on_event("startup")
+async def startup():
+    global pool
+    pool = await get_pool()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await pool.close()
+
+@app.get("/categories/")
+async def read_categories():
+    async with pool.acquire() as connection:
+        try:
+            categories = await connection.fetch("SELECT * FROM Categories")
+            return categories
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
 
-@app.post("/")
-def read_root(request: ReadRootRequest):
-    if request.name:
-        return {f"Hello, {request.name}!"}
-    else:
-        return {"Hello, World!"}
+@app.post("/categories/")
+async def create_category(request: Request):
+    data = await request.json()
+    name = data.get('name')
+    currency = data.get('currency')
+
+    if name is None or currency is None:
+        raise HTTPException(status_code=400, detail="Missing parameters")
+
+    async with pool.acquire() as connection:
+        try:
+            query = "INSERT INTO Categories (name, currency) VALUES ($1, $2) RETURNING id"
+            category_id = await connection.fetchval(query, name, currency)
+            return {"id": category_id, "name": name, "currency": currency}
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/balances/")
+async def read_balance(from_date: date = Query(...), to_date: date = Query(...)):
+    async with pool.acquire() as connection:
+        try:
+            query = """
+            SELECT
+                Balance.id,
+                Balance.cat_id,
+                Categories.name AS category_name,
+                Balance.date,
+                Balance.value,
+                Balance.rate,
+                SUM(ROUND(Balance.value * Balance.rate)/1000) AS total_amount
+            FROM Balance
+            FULL OUTER JOIN Categories ON Categories.id = Balance.cat_id
+            WHERE Balance.date >= $1 AND Balance.date <= $2
+            GROUP BY Balance.id, Balance.cat_id, Categories.name, Balance.date, Balance.value, Balance.rate
+            """
+            balances = await connection.fetch(query, from_date, to_date)
+            if balances:
+                return [{
+                    "id": balance["id"],
+                    "cat_id": balance["cat_id"],
+                    "category_name": balance["category_name"],
+                    "date": balance["date"],
+                    "value": balance["value"],
+                    "rate": balance["rate"] / 1000,
+                    "converted_value": balance["total_amount"],
+                } for balance in balances]
+            else:
+                return []
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.delete("/balance/{balance_id}")
+async def delete_balance(balance_id: int):
+    async with pool.acquire() as connection:
+        try:
+            query = "DELETE FROM Balance WHERE id = $1"
+            result = await connection.execute(query, balance_id)
+            if result == 'DELETE 0':
+                raise HTTPException(status_code=404, detail="Balance not found")
+            return {"message": "Balance deleted successfully"}
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/balance/")
+async def create_balance(request: Request):
+    data = await request.json()
+    cat_id = data.get('cat_id')
+    value = data.get('value')
+    rate = data.get('rate')
+    async with pool.acquire() as connection:
+        try:
+            insert_query = """
+            INSERT INTO Balance (cat_id, value, date, rate) 
+            VALUES ($1, $2, CURRENT_DATE, CAST($3 * 1000 AS INTEGER))
+            RETURNING id, cat_id, date, value, rate
+            """
+            balance = await connection.fetchrow(insert_query, cat_id, value, rate)
+
+            if not balance:
+                raise HTTPException(status_code=400, detail="Failed to insert balance")
+
+            category_query = "SELECT name FROM Categories WHERE id = $1"
+            category_name = await connection.fetchval(category_query, cat_id)
+
+            if not category_name:
+                raise HTTPException(status_code=404, detail="Category not found")
+
+            return {
+                    "id": balance["id"],
+                    "cat_id": balance["cat_id"],
+                    "category_name": category_name,
+                    "date": balance["date"],
+                    "value": balance["value"],
+                    "rate": balance["rate"] / 1000,
+                    "converted_value": (balance["value"] * balance["rate"]) / 1000
+            }
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
---
+++ app/postgresql_app/usecases.sql
@@ -0,0 +1,21 @@
+INSERT INTO Categories (name, currency) VALUES ('Kaspi', 'KZT'); -- Добавить новую категорию (Kaspi)
+INSERT INTO Balance (cat_id, date, value, rate) VALUES (1, '2024-06-27 13:23:00+03', 16700, CAST(0.002 * 1000 AS INTEGER)); -- Добавить баланс на определнную дату (для Kaspi)
+
+
+SELECT SUM(ROUND(value * rate)/1000) AS total_amount FROM Balance WHERE "date" = '2024-06-27 10:23:00+00'; -- Запрос для вывода суммы по всем категориям за опред. день
+SELECT SUM(ROUND(value * rate)/1000) AS total_amount FROM Balance WHERE "date"::date = current_date; --Запрос всех накоплений на текущий момент
+
+SELECT SUM(ROUND(value * rate)/1000) AS total_amount, Categories.name
+FROM Balance
+FULL OUTER JOIN Categories ON Categories.id = Balance.cat_id
+WHERE "date" >= $1 AND "date" <= $2
+GROUP BY Categories.name, Balance.value -- Запрос для вывода всех категорий по отдельности за определенный период
+
+UPDATE Balance
+SET value = 78955500
+WHERE "date"::date = current_date AND value = 78900; -- Запрос на изменение суммы в случае ошибки (если запись добавлена сегодня)
+
+DELETE FROM Balance
+WHERE "id" = 1 AND "date"::date = current_date; -- Запрос на удаление записи определенной категории (если запись добавлена сегодня)
+
+--нужно ли редактирование категории, балансы?
